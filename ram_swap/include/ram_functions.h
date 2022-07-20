2e135b518c2461bd09dd5e2a94f7ab116cd3ba55
Federico Velasco <11739480+fedevelasco@users.noreply.github.com>
2022-07-19 23:22:20 -0300

    Agrego funciones para crear las tablas de memoria para nuevos procesos

--- ram_swap/include/ram_functions.h
+++ ram_swap/include/ram_functions.h
@@ -4,9 +4,15 @@
 #include <stdbool.h>
 #include <tads/process.h>
 #include <libreriaConexiones.h>
+#include <ram_utils.h>
 
-static bool create_process_in_memory(t_process* process);
-bool process_table_exists(int32_t pid);
+bool create_process_in_memory(t_process* process);
+bool process_table_not_exists(int32_t pid);
+first_level_page_table_t* get_process_table(int32_t pid);
+second_level_page_tables_t* create_empty_second_level_page_table();
+page_t* create_empty_page();
+int32_t get_pid_first_level_table_number(int32_t pid);
+
 
 #endif /* RAM_FUNCTIONS_H_ */
 
--- ram_swap/src/ram_functions.c
+++ ram_swap/src/ram_functions.c
@@ -1,58 +1,48 @@
 #include <ram_functions.h>
 
-static bool create_process_in_memory(t_process* process) {
+bool create_process_in_memory(t_process* process) {
+
+	first_level_page_table_t* first_level_page_table;
 
 	if(process_table_not_exists(process->pid)){
 
-		first_level_page_table_t* first_level_page_table = malloc(sizeof(first_level_page_table_t));
+		log_info(logger, "Tabla de primer nivel no existe para pid: %d. Creando una nueva.", process->pid);
+
+
+		first_level_page_table = malloc(sizeof(first_level_page_table_t));
 		first_level_page_table->pid = process->pid;
-		first_level_page_table->entries = list_create();
-		}
+		first_level_page_table->entries = list_create(); //El maximo es entradas_por_tabla;
 
-	int32_t process_pages_amount = process->process_size / config.tam_pagina;
-
-	for(int i = 0; i < process_pages_amount; i++ ){
-
+	} else {
+		//TODO: Esto puede pasar? que me manden crear un proceso en memoria otra vez? lo unico que tienen que cambiar son los bit de precencia de las paginas.
+		log_info(logger, "Tabla de primer nivel ya existe para pid: %d. Deberia ser un error esto", process->pid);
+		first_level_page_table = get_process_table(process->pid);
 	}
-
-
-	second_level_page_number_t* second_level_page_number = next_second_level_page;
 
 	for (int i = 0; i < config.entradas_por_tabla; i++){
-		list_add(first_level_page_table->table, )
-		dictionary_put(, )
+
+		second_level_page_tables_t* second_level_page_table = create_empty_second_level_page_table();
+
+		int32_t second_level_page_table_number = list_add(global_page_lists->second_level_page_tables, second_level_page_table);
+
+		log_info(logger, "Se agrega tabla nivel 2 numero: %d a las entradas de tabla de nivel 1", second_level_page_table_number);
+		second_level_table_number_t* second_level_table_number = malloc(sizeof(second_level_table_number_t));
+		second_level_table_number->value = second_level_page_table_number;
+
+		list_add(first_level_page_table->entries, second_level_table_number);
+
 	}
 
-	int i;
-		for (i = 0; i < cantPaginasPedidas; i++) {
-			tablaPagina_t *nuevaPag = malloc(sizeof(tablaPagina_t));
-			int posicion;
-			if (existePidEnListadeTablas(pid)) {
-				posicion = primerNumeroPaginaLibre(pid);
-			} else {
-				posicion = i;
-			}
+	log_info(logger, "Se agrega tabla nivel 1 de pid: %d a tabla global", first_level_page_table->pid);
 
-			pthread_mutex_lock(&lock_accesoTabla);
-			nuevaPag->nroPagina = posicion;
-			nuevaPag->marcoUtilizado = -1;
-			nuevaPag->bitPresencia = 0;
-			nuevaPag->bitModificacion = 0;
-			nuevaPag->bitUso = 0;
-
-			tabla_t* tablaPag = buscarTabla(pid);
-			list_add_in_index((t_list*)tablaPag->listaPaginas, posicion, nuevaPag);
-			pthread_mutex_unlock(&lock_accesoTabla);
-		}
-		return 1;
-
+	list_add(global_page_lists->first_level_page_tables, first_level_page_table);
 
     return true;
 }
 
 
 bool process_table_not_exists(int32_t pid){
-	int size = list_size(global_page_lists->first_level_page_tables);
+	int32_t size = list_size(global_page_lists->first_level_page_tables);
 
 	for(int i = 0;i<size;i++){
 		first_level_page_table_t* table;
@@ -62,6 +52,53 @@ bool process_table_not_exists(int32_t pid){
 	}
 	return true;
 }
+
+first_level_page_table_t* get_process_table(int32_t pid){
+	int32_t size = list_size(global_page_lists->first_level_page_tables);
+
+	for(int i = 0;i<size;i++){
+		first_level_page_table_t* table;
+		table = list_get(global_page_lists->first_level_page_tables,i);
+		if(table->pid == pid)
+			return table;
+	}
+	return 0;
+}
+
+second_level_page_tables_t* create_empty_second_level_page_table(){
+	second_level_page_tables_t* second_level_page_table = malloc(sizeof(second_level_page_table));
+	second_level_page_table->pages = list_create(); //El maximo es entradas_por_tabla;
+
+	for (int i = 0; i < config.entradas_por_tabla; i++){
+		page_t* page = create_empty_page();
+		list_add(second_level_page_table->pages, page);
+	}
+
+	return second_level_page_table;
+}
+
+page_t* create_empty_page(){
+	page_t* page = malloc(sizeof(page_t));
+	page->frame_number=0;
+	page->bit_P=false;
+	page->bit_U=false;
+
+	return page;
+}
+
+int32_t get_pid_first_level_table_number(int32_t pid){
+	int32_t size = list_size(global_page_lists->first_level_page_tables);
+
+	for(int i = 0;i<size;i++){
+		first_level_page_table_t* table;
+		table = list_get(global_page_lists->first_level_page_tables,i);
+		if(table->pid == pid)
+			return i;
+	}
+	return 0;
+}
+
+
 
 
 
