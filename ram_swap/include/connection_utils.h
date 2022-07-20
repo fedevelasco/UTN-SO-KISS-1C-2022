8416ecc3adb6b48d978cf268497477df6bafaa8c
Federico Velasco <11739480+fedevelasco@users.noreply.github.com>
2022-07-19 23:25:39 -0300

    Ajustes para compilar las pruebas hasta que esten implementadas todas las funciones

--- ram_swap/include/connection_utils.h
+++ ram_swap/include/connection_utils.h
@@ -3,14 +3,18 @@
 
 #include <tads/op_code.h>
 #include <libreriaConexiones.h>
+#include <threads.h>
 #include <connection_processor.h>
+#include <ram_utils.h>
 
 typedef struct {
     int fd;
     char* server_name;
 } connection_args_t;
 
-static void process_connection(void* void_args);
+void process_connection(void* void_args);
+int server_listen_ram(char* server_name, int server_fd);
+
 
 #endif /* RAM_CONNECTION_UTILS_H_ */
 
--- ram_swap/src/connection_utils.c
+++ ram_swap/src/connection_utils.c
@@ -1,6 +1,6 @@
 #include <connection_utils.h>
 
-static void process_connection(void* void_args) {
+void process_connection(void* void_args) {
 	connection_args_t* args = (connection_args_t*) void_args;
 	int client_socket = args->fd;
 	char* server_name = args->server_name;
@@ -15,9 +15,9 @@ static void process_connection(void* void_args) {
 		}
 
 		switch (cop) {
-            case DEBUGGING:
-                log_info(logger, "Im a debug message");
-                break;
+		case DEBUGGING:
+			log_info(logger, "Im a debug message");
+			break;
 
 		case PROCESS_INIT: {
 			process_init(client_socket);
@@ -25,36 +25,31 @@ static void process_connection(void* void_args) {
 		}
 
 		case PROCESS_SUSPEND: {
-			process_suspend(client_socket);
+//			process_suspend(client_socket);
 			break;
 		}
 
 		case PROCESS_KILL: {
-			process_kill(client_socket);
+//			process_kill(client_socket);
 			break;
 		}
 		case GET_SECOND_LEVEL_TABLE: {
-			get_second_level_table(client_socket);
+//			get_second_level_table(client_socket);
 			break;
 		}
 		case GET_FRAME: {
-			get_frame(client_socket);
+//			get_frame(client_socket);
 			break;
 		}
 		case READ_MEMORY: {
-			read_memory(client_socket);
+//			read_memory(client_socket);
 			break;
 		}
 		case WRITE_MEMORY: {
-			write_memory(client_socket);
+//			write_memory(client_socket);
 			break;
 		}
 
-			// Errors
-		case -1:
-			log_error(logger,
-					"process_connection - Cliente desconectado de RAM.");
-			return;
 		default:
 			log_error(logger, "process_connection - Error en server");
 			return;
@@ -65,16 +60,15 @@ static void process_connection(void* void_args) {
 	return;
 }
 
-int server_listen_ram(char* server_name, int server_fd, t_log* logger) {
-	int client_socket = esperar_cliente(logger, server_name, server_fd);
+int server_listen_ram(char* server_name, int server_fd) {
+	int32_t client_socket = esperar_cliente(logger, server_name, server_fd);
 
 	if (client_socket != -1) {
 		pthread_t thread;
 		connection_args_t* args = malloc(sizeof(connection_args_t));
 		args->fd = client_socket;
 		args->server_name = server_name;
-		thread_create_with_parameter(&thread, NULL, (void*) process_connection,
-				(void*) args);
+		thread_create_with_parameter(&thread, (void*) process_connection, (void*) args);
 //        pthread_detach(thread); Probar, ya creo el thread como detachable. Cual uso?
 		return 1;
 	}
