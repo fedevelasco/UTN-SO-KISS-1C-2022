a396cab3e4c041f8632a05d2e37ed659c31a0eea
Federico Velasco <11739480+fedevelasco@users.noreply.github.com>
2022-07-19 23:24:49 -0300

    Completo procesamiento de comando PROCESS_INIT

--- ram_swap/include/connection_processor.h
+++ ram_swap/include/connection_processor.h
@@ -2,10 +2,16 @@
 #define RAM_CONNECTION_PROCESSOR_H_
 
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <tads/process.h>
 #include <libreriaConexiones.h>
+#include <protocol.h>
+#include <connection_utils.h>
+#include <ram.h>
+#include <ram_functions.h>
 
-
+bool process_init(int32_t client_socket);
 
 #endif /* RAM_CONNECTION_PROCESSOR_H_ */
 
--- ram_swap/src/connection_processor.c
+++ ram_swap/src/connection_processor.c
@@ -1,25 +1,39 @@
 #include <connection_processor.h>
 
-void process_init(client_socket) {
+bool process_init(int32_t client_socket) {
 
-	t_process* process = recv_process(client_socket);
+	t_process* process = malloc(sizeof(t_process));
+	process = recv_process(client_socket);
 
-	send_ack(client_socket, true);
+	//TODO: Revisar por que falla el envio.
+//	if (send_ack(client_socket, true)) {
+//		log_error(logger, "Error enviando senial de ack a Kernel");
+//		free(process);
+//		return false;
+//	}
 
 	pthread_mutex_lock(&LOCK_ACCESS_RAM);
 
 	bool ret_code = create_process_in_memory(process);
+	int32_t pid_first_level_table_number = get_pid_first_level_table_number(
+			process->pid);
 
 	pthread_mutex_unlock(&LOCK_ACCESS_RAM);
 
 	if (!ret_code) {
 		log_error(logger, "Error iniciando proceso en Memoria");
-		list_destroy_and_destroy_elements(posiciones, (void*) free);
-		free(tareas);
-		break;
+		free(process);
+		return false;
 	}
 
-	//TODO: free del pcb
+	log_info(logger, "Enviando a Kernel numero de tabla: %d del pid %d", pid_first_level_table_number, process->pid );
 
+	if (send_process_init(client_socket, pid_first_level_table_number)) {
+		log_error(logger,
+				"Error enviando numero de tabla del proceso a Kernel");
+		free(process);
+		return false;
+	}
+	return true;
 }
 
