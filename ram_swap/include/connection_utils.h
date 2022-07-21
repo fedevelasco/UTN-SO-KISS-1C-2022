60bf464b348c6f04a237c7a19c7f37b377855988
Federico Velasco <11739480+fedevelasco@users.noreply.github.com>
2022-07-21 19:48:57 -0300

    Agrego manejo de threads al crear las conexiones

--- ram_swap/include/connection_utils.h
+++ ram_swap/include/connection_utils.h
@@ -1,19 +1,34 @@
 #ifndef RAM_CONNECTION_UTILS_H_
 #define RAM_CONNECTION_UTILS_H_
 
+#include <commons/collections/queue.h>
+#include <semaphore.h>
 #include <tads/op_code.h>
 #include <libreriaConexiones.h>
-#include <threads.h>
 #include <connection_processor.h>
 #include <ram_utils.h>
 
 typedef struct {
-    int fd;
+    int socket;
     char* server_name;
 } connection_args_t;
 
+pthread_mutex_t MUTEX_KERNEL_QUEUE;
+pthread_mutex_t MUTEX_CPU_QUEUE;
+
+sem_t sem_kernel_thread;
+sem_t sem_cpu_thread;
+
+
+
+t_queue* kernel_queue;
+t_queue* cpu_queue;
+
 void process_connection(void* void_args);
 int server_listen_ram(char* server_name, int server_fd);
+int32_t kernel_opcode(op_code opcode);
+void process_kernel_functions();
+void process_cpu_functions();
 
 
 #endif /* RAM_CONNECTION_UTILS_H_ */
--- ram_swap/src/connection_utils.c
+++ ram_swap/src/connection_utils.c
@@ -1,77 +1,200 @@
 #include <connection_utils.h>
 
-void process_connection(void* void_args) {
-	connection_args_t* args = (connection_args_t*) void_args;
-	int client_socket = args->fd;
-	char* server_name = args->server_name;
-	free(args);
+//void process_connection(void* void_args) {
+//	connection_args_t* args = (connection_args_t*) void_args;
+//	int client_socket = args->socket;
+//	char* server_name = args->server_name;
+//	free(args);
+//
+//	op_code cop;
+//	while (client_socket != -1) {
+//
+//		if (recv(client_socket, &cop, sizeof(op_code), 0) != sizeof(op_code)) {
+//			log_info(logger, "process_connection - Error recibiendo conexion");
+//			return;
+//		}
+//
+//		switch (cop) {
+//		case DEBUGGING:
+//			log_info(logger, "Im a debug message");
+//			break;
+//
+//		case PROCESS_INIT: {
+//			process_init(client_socket);
+//			break;
+//		}
+//
+//		case PROCESS_SUSPEND: {
+////			process_suspend(client_socket);
+//			break;
+//		}
+//
+//		case PROCESS_KILL: {
+////			process_kill(client_socket);
+//			break;
+//		}
+//		case GET_SECOND_LEVEL_TABLE: {
+////			get_second_level_table(client_socket);
+//			break;
+//		}
+//		case GET_FRAME: {
+////			get_frame(client_socket);
+//			break;
+//		}
+//		case READ_MEMORY: {
+////			read_memory(client_socket);
+//			break;
+//		}
+//		case WRITE_MEMORY: {
+////			write_memory(client_socket);
+//			break;
+//		}
+//
+//		default:
+//			log_error(logger, "process_connection - Error en server");
+//			return;
+//		}
+//	}
+//
+//	log_warning(logger, "El cliente se desconecto de %s server", server_name);
+//	return;
+//}
 
-	op_code cop;
-	while (client_socket != -1) {
+int server_listen_ram(char* server_name, int server_socket) {
 
-		if (recv(client_socket, &cop, sizeof(op_code), 0) != sizeof(op_code)) {
-			log_info(logger, "process_connection - Error recibiendo conexion");
-			return;
-		}
 
-		switch (cop) {
-		case DEBUGGING:
-			log_info(logger, "Im a debug message");
-			break;
+	int32_t client_socket = esperar_cliente(logger, server_name, server_socket);
+	while(1){
 
-		case PROCESS_INIT: {
-			process_init(client_socket);
-			break;
-		}
 
-		case PROCESS_SUSPEND: {
-//			process_suspend(client_socket);
-			break;
-		}
-
-		case PROCESS_KILL: {
-//			process_kill(client_socket);
-			break;
-		}
-		case GET_SECOND_LEVEL_TABLE: {
-//			get_second_level_table(client_socket);
-			break;
-		}
-		case GET_FRAME: {
-//			get_frame(client_socket);
-			break;
-		}
-		case READ_MEMORY: {
-//			read_memory(client_socket);
-			break;
-		}
-		case WRITE_MEMORY: {
-//			write_memory(client_socket);
-			break;
-		}
-
-		default:
-			log_error(logger, "process_connection - Error en server");
-			return;
-		}
-	}
-
-	log_warning(logger, "El cliente se desconecto de %s server", server_name);
-	return;
-}
-
-int server_listen_ram(char* server_name, int server_fd) {
-	int32_t client_socket = esperar_cliente(logger, server_name, server_fd);
 
 	if (client_socket != -1) {
-		pthread_t thread;
-		connection_args_t* args = malloc(sizeof(connection_args_t));
-		args->fd = client_socket;
-		args->server_name = server_name;
-		thread_create_with_parameter(&thread, (void*) process_connection, (void*) args);
-//        pthread_detach(thread); Probar, ya creo el thread como detachable. Cual uso?
-		return 1;
+
+		op_code opcode;
+		if (recv(client_socket, &opcode, sizeof(op_code), 0) != sizeof(op_code)) {
+			log_error(logger, "server_listen_ram - Error recibiendo op_code");
+			return 0;
+		}
+
+		int32_t buffer_size;
+		char* buffer;
+
+		buffer = recv_buffer(&buffer_size, client_socket);
+
+		operation_buffer_t* operation_buffer = malloc(sizeof(operation_buffer_t));
+		operation_buffer->opcode = opcode;
+		operation_buffer->buffer = buffer;
+
+		if(kernel_opcode(opcode)){
+			pthread_mutex_lock(&MUTEX_KERNEL_QUEUE);
+			queue_push(kernel_queue, operation_buffer);
+			pthread_mutex_unlock(&MUTEX_KERNEL_QUEUE);
+			sem_post(&sem_kernel_thread);
+		} else {
+			pthread_mutex_lock(&MUTEX_CPU_QUEUE);
+			queue_push(cpu_queue, operation_buffer);
+			pthread_mutex_unlock(&MUTEX_CPU_QUEUE);
+			sem_post(&sem_cpu_thread);
+		}
+
+	}
+
 	}
 	return 0;
+}
+
+int32_t kernel_opcode(op_code opcode){
+
+	if(
+		opcode == PROCESS_INIT_REQUEST ||
+		opcode == PROCESS_INIT_SEND ||
+		opcode == PROCESS_SUSPEND_REQUEST ||
+		opcode == PROCESS_SUSPEND_SEND ||
+		opcode == PROCESS_KILL_REQUEST ||
+		opcode == PROCESS_KILL_SEND
+		){
+		log_info(logger, "kernel_opcode - El opcode es de Kernel");
+		return 1;
+	}
+
+	return 0;
+}
+
+void process_kernel_functions(){
+	while(true){
+	        sem_wait(&sem_kernel_thread);
+	        pthread_mutex_lock(&MUTEX_KERNEL_QUEUE);
+	        operation_buffer_t* operation_buffer = queue_pop(kernel_queue);
+	        pthread_mutex_unlock(&MUTEX_KERNEL_QUEUE);
+
+	        switch (operation_buffer->opcode) {
+	        		case DEBUGGING: {
+	        			log_info(logger, "Im a debug message");
+	        			break;
+	        		}
+	        		case PROCESS_INIT_REQUEST: {
+	        			process_init(operation_buffer->buffer);
+	        			break;
+	        		}
+
+	        		case PROCESS_SUSPEND_REQUEST: {
+	        //			process_suspend(operation_buffer->buffer);
+	        			break;
+	        		}
+
+	        		case PROCESS_KILL_REQUEST: {
+	        //			process_kill(operation_buffer->buffer);
+	        			break;
+	        		}
+
+	        		default:
+	        			log_error(logger, "process_kernel_functions - Error en server");
+	        			return;
+	        		}
+
+	        free(operation_buffer->buffer);
+	        free(operation_buffer);
+	    }
+
+}
+
+void process_cpu_functions(){
+	while(true){
+		        sem_wait(&sem_cpu_thread);
+		        pthread_mutex_lock(&MUTEX_CPU_QUEUE);
+		        operation_buffer_t* operation_buffer = queue_pop(cpu_queue);
+		        pthread_mutex_unlock(&MUTEX_KERNEL_QUEUE);
+
+		        switch (operation_buffer->opcode) {
+		        		case DEBUGGING: {
+		        			log_info(logger, "Im a debug message");
+		        			break;
+		        		}
+		        		case GET_SECOND_LEVEL_TABLE_REQUEST: {
+		        //			get_second_level_table(operation_buffer->buffer);
+		        			break;
+		        		}
+		        		case GET_FRAME_REQUEST: {
+		        //			get_frame(operation_buffer->buffer);
+		        			break;
+		        		}
+		        		case READ_MEMORY_REQUEST: {
+		        //			read_memory(operation_buffer->buffer);
+		        			break;
+		        		}
+		        		case WRITE_MEMORY_REQUEST: {
+		        //			write_memory(operation_buffer->buffer);
+		        			break;
+		        		}
+
+		        		default:
+		        			log_error(logger, "process_cpu_functions - Error en server");
+		        			return;
+		        		}
+
+		        free(operation_buffer->buffer);
+		        free(operation_buffer);
+		    }
+
 }
 
