451a938032457d2752182e8e28741a30aaa7cdc2
Federico Velasco <11739480+fedevelasco@users.noreply.github.com>
2022-07-21 19:46:26 -0300

    Agrego config para levantar ips. Ajustes a variables globales de config para usarlas mas facil. Inicializacion de mutex, threads y semaforos.

--- ram_swap/include/ram_utils.h
+++ ram_swap/include/ram_utils.h
@@ -1,17 +1,19 @@
 #ifndef RAM_UTILS_H_
 #define RAM_UTILS_H_
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
 #include <pthread.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<netdb.h>
-#include<string.h>
-#include<commons/log.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <math.h>
+#include <string.h>
+#include <commons/log.h>
 #include <commons/collections/list.h>
-#include<commons/config.h>
+#include <commons/bitarray.h>
+#include <commons/config.h>
 
 #include <tads/parameter.h>
 #include <tads/instruction.h>
@@ -20,73 +22,106 @@
 #include <tads/package.h>
 #include <tads/buffer.h>
 #include <serialization.h>
+#include <connection_utils.h>
 
-typedef struct custom_config {
-	int32_t puerto_escucha;
-	int32_t tam_memoria;
-	int32_t tam_pagina;
-	int32_t entradas_por_tabla;
-	int32_t retardo_memoria;
-	char* 	algoritmo_reemplazo;
-	int32_t marcos_por_proceso;
-	int32_t retardo_swap;
-	char*   path_swap;
+// Mutex
 
-} custom_config_t;
+pthread_mutex_t MUTEX_MEMORY;
+pthread_mutex_t MUTEX_SWAP;
+pthread_mutex_t MUTEX_FIRST_LEVEL_TABLES;
+pthread_mutex_t MUTEX_SECOND_LEVEL_TABLES;
+pthread_mutex_t MUTEX_SECOND_LEVEL_ENTRY;
+pthread_mutex_t MUTEX_PROCESS_EXTRA_INFO;
+pthread_mutex_t MUTEX_ID_EN_SWAP;
+pthread_mutex_t MUTEX_OCCUPIED_FRAMES;
 
-typedef struct second_level_table_number {
-	int32_t value;
-} second_level_table_number_t ;
 
-typedef struct global_page_lists {
-
-	//Primer Nivel
-	t_list* first_level_page_tables;
-
-	//Segundo Nivel
-	t_list* second_level_page_tables;
-
-} global_page_lists_t;
-
+// Tabla de paginas primer nivel
 typedef struct first_level_page_table {
 
 	int32_t pid;
-	t_list* entries; //El maximo es entradas_por_tabla;
+	t_list* first_level_entries; //first_level_entries_t
 
 } first_level_page_table_t;
 
-typedef struct second_level_page_tables{
+typedef struct first_level_entries {
+	int32_t id;
+	int32_t second_level_table_id;
+} first_level_entries_t;
 
-	t_list* pages; //El maximo es entradas_por_tabla;
-} second_level_page_tables_t;
+// Tabla de paginas segundo nivel
+typedef struct second_level_page_table{
+
+	t_list* pages; // page_t
+} second_level_page_table_t;
 
 typedef struct page {
-    //page_number es el index de la lista
+    int32_t id;
     int32_t frame_number;
     bool bit_U;     // bit de uso
     bool bit_P;     // bit de presencia
+    bool bit_M;     // bit de modificado
+    int32_t swap_page_id;
 } page_t;
 
+typedef struct process_state{
+        int32_t frames_used;
+        int32_t clock_pointer;
+        int32_t first_level_page_table_id;
+    } process_state_t;
+
+typedef struct operation_buffer{
+	op_code opcode;
+	char* buffer;
+} operation_buffer_t;
+
+// Threads
+
+pthread_t kernel_thread;
+pthread_t cpu_thread;
+
 // Variables globales
+char* puerto_escucha;
+int32_t tam_memoria;
+int32_t tam_pagina;
+int32_t entradas_por_tabla;
+int32_t retardo_memoria;
+char* 	algoritmo_reemplazo;
+int32_t marcos_por_proceso;
+int32_t retardo_swap;
+char*   path_swap;
+char* ip;
+int32_t cantidad_frames;
+
 
 t_log* logger;
-custom_config_t config;
-int32_t retardo_memoria;
+
 t_list* last_memory_position_used;
-int32_t memory_size;
+
 void* memory;
-int32_t* occupied_frames_vector;
-global_page_lists_t* global_page_lists;
+t_bitarray* occupied_frames_bitarray;
+t_dictionary* process_extra_info;
+
+// Listas de paginas globales
+t_list* global_first_level_page_tables;
+t_list* global_second_level_page_tables;
+
+
 
 
 
 // Funciones
 t_log* start_logger(void);
-int32_t load_ram_configuration_file(t_log* logger);
-int32_t memory_create(custom_config_t config, t_log* logger);
-void occupied_frames_vector_create(custom_config_t config, t_log* logger);
-int32_t paging_create(custom_config_t config, t_log* logger);
+int32_t load_ram_configuration_file(char* config_path, char* ip_config_path);
+int32_t memory_create();
+void occupied_frames_vector_create();
+int32_t paging_tables_create();
 void close_process();
+char* create_bitarray_memory_block(int32_t frames);
+int32_t threads_init();
+int32_t mutex_init();
+int32_t aux_elements_init();
+
 
 
 
--- ram_swap/src/ram_utils.c
+++ ram_swap/src/ram_utils.c
@@ -1,12 +1,11 @@
 #include <ram_utils.h>
 
-t_log* start_logger(void)
-{
+t_log* start_logger(void) {
 	t_log* new_logger;
 
 	new_logger = log_create("ram_swap.log", "Ram_Swap", 1, LOG_LEVEL_DEBUG);
 
-	if(new_logger == NULL){
+	if (new_logger == NULL) {
 		printf("No fue posible crear el Logger\n");
 		exit(1);
 	}
@@ -14,84 +13,169 @@ t_log* start_logger(void)
 	return new_logger;
 }
 
-int32_t load_ram_configuration_file(t_log* logger)
-{
-	log_info(logger, "Carga de archivo de configuracion - load_configuration_file - Inicio");
+int32_t load_ram_configuration_file(char* config_path, char* ip_config_path) {
+	log_info(logger,"Carga de archivo de configuracion - load_configuration_file - Inicio");
 	t_config* config_to_load;
+	t_config* ip_config_to_load;
 
-	config_to_load = config_create("../ram_swap/ram_swap.config");
 
-	if (config_to_load == NULL){
-		log_error(logger, "Carga de archivo de configuracion - load_configuration_file - Error al crear configuracion");
-		return 0;
+	config_to_load = config_create(config_path); // "../ram_swap/ram_swap.config"
+	if (config_to_load == NULL) {
+		log_error(logger,"Carga de archivo de configuracion - load_configuration_file - Error al crear configuracion - path: %s",config_path);
+		return 1;
+	}
+
+	ip_config_to_load = config_create(ip_config_path); // "../ram_swap/ips.config"
+	if (ip_config_to_load == NULL) {
+		log_error(logger,"Carga de archivo de configuracion - load_configuration_file - Error al crear configuracion de ips - path: %s",ip_config_path);
+		return 1;
 	}
 
 	//Almaceno valores de config en custom_config_t
-	config.puerto_escucha = config_get_int_value(config_to_load, "PUERTO_ESCUCHA");
-	config.tam_memoria = config_get_int_value(config_to_load, "TAM_MEMORIA");
-	config.tam_pagina = config_get_int_value(config_to_load, "TAM_PAGINA");
-	config.entradas_por_tabla = config_get_int_value(config_to_load, "ENTRADAS_POR_TABLA");
-	config.retardo_memoria = config_get_int_value(config_to_load, "RETARDO_MEMORIA");
-	config.algoritmo_reemplazo = config_get_string_value(config_to_load, "ALGORITMO_REEMPLAZO");
-	config.marcos_por_proceso = config_get_int_value(config_to_load, "MARCOS_POR_PROCESO");
-	config.retardo_swap = config_get_int_value(config_to_load, "RETARDO_SWAP");
-	config.path_swap = config_get_string_value(config_to_load, "PATH_SWAP");
+	tam_memoria = config_get_int_value(config_to_load, "TAM_MEMORIA");
+	tam_pagina = config_get_int_value(config_to_load, "TAM_PAGINA");
+	entradas_por_tabla = config_get_int_value(config_to_load, "ENTRADAS_POR_TABLA");
+	retardo_memoria = config_get_int_value(config_to_load, "RETARDO_MEMORIA");
+	algoritmo_reemplazo = config_get_string_value(config_to_load, "ALGORITMO_REEMPLAZO");
+	marcos_por_proceso = config_get_int_value(config_to_load, "MARCOS_POR_PROCESO");
+	retardo_swap = config_get_int_value(config_to_load, "RETARDO_SWAP");
+	path_swap = config_get_string_value(config_to_load, "PATH_SWAP");
+	ip = config_get_string_value(ip_config_to_load, "IP_RAM");
+	puerto_escucha = config_get_string_value(ip_config_to_load, "PUERTO_ESCUCHA_RAM");
+	cantidad_frames = tam_memoria / tam_pagina;
+	strin
 
-	config_destroy(config_to_load);
 
-	log_info(logger, "Carga de archivo de configuracion - load_configuration_file - Config en %s cargada ok", config_to_load->path);
 
-	return 1;
+	log_info(logger,"Carga de archivo de configuracion - load_configuration_file - Config en %s y en %s cargadas ok", config_path, ip_config_path);
+
+	return 0;
 }
 
-int32_t memory_create(custom_config_t config, t_log* logger){
+int32_t memory_create() {
 
 	//Creo memoria y la relleno
-	memory_size= config.tam_memoria;
-	memory = malloc(memory_size);
+	memory = malloc(tam_memoria);
 	if (memory == NULL) {
-	        log_error(logger, "Fallo en el malloc de memory");
-	        return 0;
-	    }
-	memset(memory,'\0',memory_size);
+		log_error(logger, "memory_create - Fallo en el malloc de memory");
+		return 1;
+	}
+	memset(memory, '\0', tam_memoria);
 
-	retardo_memoria = config.retardo_memoria;
+	//Creo bitarray para saber frames libres
+
+	int32_t frames = cantidad_frames / 8;
+	char *bitarray_memory_block = create_bitarray_memory_block(cantidad_frames);
+    occupied_frames_bitarray = bitarray_create_with_mode(bitarray_memory_block, frames, LSB_FIRST);
+
+	for (int i = 0; i < cantidad_frames; i++){
+		bitarray_clean_bit(occupied_frames_bitarray, i);
+	}
 
 	last_memory_position_used = list_create();
 
-	occupied_frames_vector_create(config, logger);
+	process_extra_info = dictionary_create();
 
-	log_info(logger,"Creada la memoria");
+	log_info(logger, "memory_create - Memoria creada con exito");
 
-	return 1;
+	return 0;
+}
+
+char* create_bitarray_memory_block(int32_t frames){
+
+    // Si cantidad frames menor a 8, ocupa 1 byte. Si es mayor ocupa, el entero superior.
+    int32_t size = frames < 8 ? 1 : (int32_t) ceil( (double)frames / 8.0 );
+
+    char* memory_block = malloc(size);
+    memset(memory_block, 0, size);
+
+    return memory_block;
+
 
 }
 
-void occupied_frames_vector_create(custom_config_t config, t_log* logger){
 
-	int32_t frames = config.tam_memoria / config.tam_pagina;
+int32_t paging_tables_create() {
 
-	//Creo vector de marcos ocupados y lo relleno
-	occupied_frames_vector = malloc(sizeof(int32_t) * frames);
+	global_first_level_page_tables = list_create();
+	global_second_level_page_tables = list_create();
 
-	memset(occupied_frames_vector,0,sizeof(int32_t) * frames);
+	log_info(logger, "paging_tables_create - Tablas de paginas globales creadas con exito");
 
-}
-
-int32_t paging_create(custom_config_t config, t_log* logger){
-
-	global_page_lists = malloc(sizeof(global_page_lists_t));
-
-	global_page_lists->first_level_page_tables = list_create();
-	global_page_lists->second_level_page_tables = list_create();
-
-	return 1;
+	return 0;
 
 }
+
+int32_t mutex_init(){
+
+
+	if (
+	pthread_mutex_init(&MUTEX_MEMORY, (void *)NULL) ||
+	pthread_mutex_init(&MUTEX_SWAP, (void *)NULL) ||
+	pthread_mutex_init(&MUTEX_FIRST_LEVEL_TABLES, (void *)NULL) ||
+	pthread_mutex_init(&MUTEX_SECOND_LEVEL_TABLES, (void *)NULL) ||
+	pthread_mutex_init(&MUTEX_SECOND_LEVEL_ENTRY, (void *)NULL) ||
+	pthread_mutex_init(&MUTEX_PROCESS_EXTRA_INFO, (void *)NULL) ||
+	pthread_mutex_init(&MUTEX_ID_EN_SWAP, (void *)NULL) ||
+	pthread_mutex_init(&MUTEX_OCCUPIED_FRAMES, (void *)NULL)
+	)
+	{
+		log_error(logger, "mutex_init - Error en creacion de mutex");
+
+		return 1;
+	}
+
+	log_info(logger, "mutex_init - Mutex creados con exito");
+	return 0;
+}
+
+int32_t threads_init(){
+
+	 if(pthread_create(&kernel_thread, NULL, (void*)process_kernel_functions, NULL)){
+		 log_error(logger, "threads_init - Error en creacion de thread de kernel");
+		 return 1;
+	 }
+	 pthread_detach(kernel_thread);
+
+
+	 if(pthread_create(&cpu_thread, NULL, (void*) process_cpu_functions, NULL)){
+		 log_error(logger, "threads_init - Error en creacion de thread de cpu");
+		 return 1;
+	 }
+	 pthread_detach(cpu_thread);
+
+	 return 0;
+}
+
+int32_t aux_elements_init(){
+
+	if(pthread_mutex_init(&MUTEX_KERNEL_QUEUE, (void *)NULL)){
+		log_error(logger, "aux_elements_init - Error en creacion de mutex de cola de kernel");
+		 return 1;
+	}
+
+	if(pthread_mutex_init(&MUTEX_CPU_QUEUE, (void *)NULL)){
+		log_error(logger, "aux_elements_init - Error en creacion de mutex de cola de cpu");
+		 return 1;
+	}
+
+    sem_init(&sem_kernel_thread, 0, 0);
+    sem_init(&sem_cpu_thread, 0, 0);
+
+    kernel_queue = queue_create();
+    cpu_queue = queue_create();
+
+    return 0;
+}
+
+
+
 
 
 void close_process() {
 	//TODO: REVISAR
+//	config_destroy(config_to_load);
+//	config_destroy(ip_config_to_load);
 //	destruirLogs();
 //	log_destroy(dump);
 //	list_destroy(listaTablasPaginas);
@@ -105,7 +189,4 @@ void close_process() {
 //	pthread_mutex_destroy(&lock_accesoTlb);
 //	pthread_mutex_destroy(&lock_accesoUltimaPos);
 }
-
-
-
 
