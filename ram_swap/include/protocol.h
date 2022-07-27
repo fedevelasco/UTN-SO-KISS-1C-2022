7ae43592f9b5ec1049f2df85d305d7a69527df3c
Federico Velasco <11739480+fedevelasco@users.noreply.github.com>
2022-07-26 21:31:06 -0300

    Agrego funciones para enviar respuesta de memory_config

--- ram_swap/include/protocol.h
+++ ram_swap/include/protocol.h
@@ -11,13 +11,15 @@
 
 
 
-char* recv_buffer(int32_t* buffer_size, int32_t client_socket);
+char* recv_buffer(int32_t client_socket);
 int32_t send_process_init(int32_t client_socket, int32_t pid_first_level_table_number);
 int32_t send_package(int32_t connection, t_package* package);
 int32_t send_to_server(int32_t connection, t_package* package);
 t_buffer* new_process_init_buffer(int32_t pid_first_level_table_number);
 int32_t send_process_suspend(int32_t client_socket);
 t_buffer* new_process_suspend_buffer();
+int32_t send_memory_config(int32_t client_socket, t_memory_config* memory_config);
+t_buffer* new_memory_config_buffer(t_memory_config* memory_config);
 
 
 
--- ram_swap/src/protocol.c
+++ ram_swap/src/protocol.c
@@ -1,13 +1,13 @@
 #include <protocol.h>
 
 
-char* recv_buffer(int32_t* buffer_size, int32_t client_socket)
+char* recv_buffer(int32_t client_socket)
 {
 	char* buffer;
-
-	read(client_socket, buffer_size, sizeof(int32_t));
-	buffer = malloc(*buffer_size);
-	read(client_socket, buffer, *buffer_size);
+	int32_t buffer_size;
+	recv(client_socket, &buffer_size, sizeof(int32_t), 0);
+	buffer = malloc(buffer_size);
+	recv(client_socket, buffer, buffer_size, 0);
 
 	return buffer;
 }
@@ -24,7 +24,7 @@ int32_t send_process_init(int32_t client_socket, int32_t pid_first_level_table_n
 		return 1;
 	}
 
-	log_debug(logger, "process_init - se envio el paquete a servidor");
+	log_debug(logger, "send_process_init - se envio el paquete a servidor");
 	return 0;
 }
 
@@ -39,7 +39,22 @@ int32_t send_process_suspend(int32_t client_socket){
 		return 1;
 	}
 
-	log_debug(logger, "process_init - se envio el paquete a servidor");
+	log_debug(logger, "send_process_suspend - se envio el paquete a servidor");
+	return 0;
+}
+
+int32_t send_memory_config(int32_t client_socket, t_memory_config* memory_config){
+
+	t_buffer* buffer = new_memory_config_buffer(memory_config);
+
+	t_package* package = new_package(buffer, GET_MEMORY_CONFIG_RESPONSE);
+
+	if (send_to_server(client_socket, package) == -1) {
+		log_error(logger, "Error al enviar paquete al servidor");
+		return 1;
+	}
+
+	log_debug(logger, "send_memory_config - se envio el paquete a servidor");
 	return 0;
 }
 
@@ -96,6 +111,20 @@ t_buffer* new_process_suspend_buffer(){
 	int offset = serialize_int(buffer->stream, &ok_signal);
 
 	log_debug(logger, "new_process_suspend_buffer - size: %d\n", offset);
+
+	return buffer;
+}
+
+t_buffer* new_memory_config_buffer(t_memory_config* memory_config){
+
+	t_buffer* buffer = create_buffer();
+
+	buffer->size = sizeof(int32_t);
+
+	buffer->stream = malloc(buffer->size);
+	int offset = serialize_memory_config(buffer->stream, memory_config);
+
+	log_debug(logger, "new_memory_config_buffer - size: %d\n", offset);
 
 	return buffer;
 }
