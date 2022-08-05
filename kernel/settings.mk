18dfed859ffcef122a956d9a8272ebd94f356ae5
Federico Velasco <11739480+fedevelasco@users.noreply.github.com>
2022-08-05 20:46:27 -0300

    Ajuste de path

--- kernel/settings.mk
+++ kernel/settings.mk
@@ -10,15 +10,15 @@ CDEBUG=-g -Wall -DDEBUG
 CRELEASE=-O3 -Wall -DNDEBUG
 
 # Arguments when executing with start, memcheck or helgrind
-ARGS=/home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/BASE_KERNEL.cfg /home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
+ARGS=/home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/BASE_KERNEL.cfg /home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
 
 #Argumentos pruebas
-BASE=/home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/BASE_KERNEL.cfg /home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
-PLANIFICACION=/home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/PLANI_KERNEL.cfg /home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
-SUSPENSION=/home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/SUSPE_KERNEL.cfg /home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
-MEMORIA=/home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/MEMORIA_KERNEL.cfg /home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
-TLB=/home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/TLB_KERNEL.cfg /home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
-INTEGRAL=/home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/INTEGRAL_KERNEL.cfg /home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
+BASE=/home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/BASE_KERNEL.cfg /home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
+PLANIFICACION=/home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/PLANI_KERNEL.cfg /home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
+SUSPENSION=/home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/SUSPE_KERNEL.cfg /home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
+MEMORIA=/home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/MEMORIA_KERNEL.cfg /home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
+TLB=/home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/TLB_KERNEL.cfg /home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
+INTEGRAL=/home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/INTEGRAL_KERNEL.cfg /home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
 
 # Valgrind flags
 MEMCHECK_FLAGS=--track-origins=yes --log-file="memcheck.log" --show-leak-kinds=all
--- ram_swap/settings.mk
+++ ram_swap/settings.mk
@@ -10,15 +10,15 @@ CDEBUG=-g -Wall -DDEBUG
 CRELEASE=-O3 -Wall -DNDEBUG
 
 # Arguments when executing with start, memcheck or helgrind
-ARGS=/home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/ram_swap/ram_swap.config /home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/ram_swap/ips.config
+ARGS=/home/utnso/tp-2022-1c-Grupo-TP-SO/ram_swap/ram_swap.config /home/utnso/tp-2022-1c-Grupo-TP-SO/ram_swap/ips.config
 
 #Argumentos para pruebas
-BASE=/home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/BASE_MEMORIA.cfg /home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
-PLANIFICACION=/home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/PLANI_MEMORIA.cfg /home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
-SUSPENSION=/home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/SUSPE_MEMORIA.cfg /home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
-MEMORIA=/home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/MEMORIA_MEMORIA.cfg /home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
-TLB=/home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/TLB_MEMORIA.cfg /home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
-INTEGRAL=/home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/INTEGRAL_MEMORIA.cfg /home/utnso/shared/git/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
+BASE=/home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/BASE_MEMORIA.cfg /home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
+PLANIFICACION=/home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/PLANI_MEMORIA.cfg /home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
+SUSPENSION=/home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/SUSPE_MEMORIA.cfg /home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
+MEMORIA=/home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/MEMORIA_MEMORIA.cfg /home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
+TLB=/home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/TLB_MEMORIA.cfg /home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
+INTEGRAL=/home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/INTEGRAL_MEMORIA.cfg /home/utnso/tp-2022-1c-Grupo-TP-SO/pruebas/IP.cfg
 
 # Valgrind flags
 MEMCHECK_FLAGS=--track-origins=yes --log-file="memcheck.log" --show-leak-kinds=all
